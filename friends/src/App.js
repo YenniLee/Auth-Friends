f91776d08492fa217c092053bef29f5f363b139b
Jennie Van <missjenvan@gmail.com>
2019-11-12 13:58:15 -0800

    changed nav, added logout

--- friends/src/App.js
+++ friends/src/App.js
@@ -11,9 +11,11 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <h1>Authentican Friends App</h1>
         <div className='nav'>
           <NavLink to='/login'>Login</NavLink>
           <NavLink to='/protected'>Friends List</NavLink>
+          <NavLink to ='/'>Log Out</NavLink>
         </div>
         <Switch>
           <PrivateRoute exact path='/protected' component={FriendsList} />
