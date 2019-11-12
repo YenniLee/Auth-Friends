7abcd9f361f0b89a53eefb5e1593bccfa2abcf10
Jennie Van <missjenvan@gmail.com>
2019-11-12 13:44:12 -0800

    delete functional

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -22,7 +22,7 @@ function FriendsList() {
       <h2>FriendsList</h2>
       <div className='friend-box'>
         {friends.map(friend => (
-          <Friend friend={friend} />
+          <Friend friend={friend} setFriends={setFriends} />
         ))}
       </div>
     </div>
